da85663f4b93c28b74c6803e2d924d64b82278e8
Benoit, Johannes <jbenoit@t-online.de>
2024-09-29 15:23:52 +0200

    small changes

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # osmand_rendering
 rendering files for osmand app
+
+
+#lines
+
+
+##simple line
+
+color="#ffffff" 
+strokeWidth="6"
+
+--> white line 6 broad
+
+## line with colored border
+color="#ffffff" color_0="#837DF6" 
+strokeWidth="4" strokeWidth_0="6"
+
+--> white line (4) over a blue line (6)
+
+
+color="#ffffff" color_0="#837DF6" 
+strokeWidth="4" strokeWidth_0="4:5" pathEffect="4_12"
+
+
+
+        <!--
+        <switch>
+            <case tag="highway" value="track"/>
+            <apply color="#ffffff"  color_0="#837DF6" order="2"/> 
+            <groupFilter>
+                <filter maxzoom="16" strokeWidth="4" strokeWidth_0="4:5" pathEffect="8_24"/>
+                <filter minzoom="16" strokeWidth="8" strokeWidth_0="8:9" pathEffect="12_36"/>
+            
+        
+            </groupFilter>
+        </switch>
+        -->
--- tests/test_parse_and_build.py
+++ tests/test_parse_and_build.py
@@ -6,15 +6,16 @@ import codecs
 import json
 import zipfile
 
-PHONE_PATH = '/run/user/1000/gvfs/mtp:host=wheatek_BV8800_BV8800EEA0022112/Interner gemeinsamer Speicher/Documents/obb/net.osmand/rendering'
+PHONE_PATH = '/run/user/1000/gvfs/mtp:host=wheatek_BV8800_BV8800EEA0022112/Interner gemeinsamer Speicher/Documents/net.osmand/rendering'
 
 
 class ParseTests(unittest.TestCase):
-    """ just runs a parse of the rendering.xml file """
+    """ just runs a parse of the rendering.xml file
+        creates a osmand import file for the xml ( .osf file)
+        uploads the xml to a phone to the path above for testing """
     # -----------------------------------------------------------------------
 
     def setUp(self):
-        print(os.getcwd())
         self.workDir = '..'
         self.buildDir = os.path.join(self.workDir, 'build')
         if not os.path.exists(self.buildDir):
@@ -27,18 +28,25 @@ class ParseTests(unittest.TestCase):
 
     # -----------------------------------------------------------------------
 
-    def testParseAndBuildOSF(self):
-
+    def _getXmlFileList(self):
+        fileList = []
         for fname in os.listdir(self.workDir):
-            base, ext = os.path.splitext(fname)
+            baseName, ext = os.path.splitext(fname)
+            fPath = os.path.join(self.workDir, fname)
             if ext != '.xml':
                 continue
-            print('parsing: %s' % fname)
-            fPath = os.path.join(self.workDir, fname)
+            fileList.append((fname, fPath, baseName))
+        return fileList
+
+    def test_1_parseXml(self):
+
+        for fname, fPath, baseName in self._getXmlFileList():
             etree.parse(fPath)
 
+    def test_2_buildOSF(self):
+        for fname, fPath, baseName in self._getXmlFileList():
             # BUILD THE OSF FILE
-            osfPath = os.path.join(self.buildDir, 'items.json')
+            osfManifestPath = os.path.join(self.buildDir, 'items.json')
             items_dict = {
                 "version": 1,
                 "items": [
@@ -50,15 +58,18 @@ class ParseTests(unittest.TestCase):
                 ]
             }
 
-            styleName = base.replace('.render', '')
+            styleName = baseName.replace('.render', '')
             j = json.dumps(items_dict, indent=4)
-            open(osfPath, 'w').write(j)
+            open(osfManifestPath, 'w').write(j)
             zipPath = os.path.join(self.buildDir, '%s.osf' % styleName)
             zip_file = zipfile.ZipFile(zipPath, 'w')
             zip_file.write(fPath, arcname='rendering/%s' % fname)
-            zip_file.write(osfPath, arcname='items.json')
+            zip_file.write(osfManifestPath, arcname='items.json')
             zip_file.close()
 
+    def test_3_uploadXmlToPhone(self):
+
+        for fname, fPath, baseName in self._getXmlFileList():
             # if exists, copy to phone.
             if os.path.exists(PHONE_PATH):
                 with codecs.open(fPath, mode='r', encoding='utf-8') as fp:
